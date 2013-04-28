e81ca9c3078181f2fc1ab87be90340b85ec85d52
tedder <ted@adhoclabs.co>
2013-04-28 11:44:51 -0700

    .

---
+++ wikibot_all_project_pages/recipes/configure.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: wikibot_all_project_pages
+# Recipe:: default
+#
+# Copyright 2013, Ted Timmons, MIT license
+#
+
+package "perl"
+
+# Time::ParseDate
+package "libtime-modules-perl"
+
+# MediaWiki::API
+package "libmediawiki-api-perl"
+
