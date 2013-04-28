545d8f064c0200c2f1911b86cf23093a4cff6e3b
tedder <ted@adhoclabs.co>
2013-04-27 20:38:05 -0700

    fix up recipe fails

--- wikibot_all_project_pages/recipes/default.rb
+++ wikibot_all_project_pages/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2013, Ted Timmons, MIT license
 #
 
-include_recipe 'perl'
+package 'perl'
 package 'libtime-modules-perl' # Time::ParseDate
 package 'libmediawiki-api-perl' # MediaWiki::API
 
