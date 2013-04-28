78790e64d8d9a74f0a6ec37801f705643c37cb75
tedder <ted@adhoclabs.co>
2013-04-28 14:00:12 -0700

    create dir

--- wikibot_all_project_pages/recipes/deploy_wikibacon.rb
+++ wikibot_all_project_pages/recipes/deploy_wikibacon.rb
@@ -1,3 +1,9 @@
+directory "/opt/wikibacon" do
+	owner "ubuntu"
+	group "ubuntu"
+	mode 0755
+	action :create
+end
 
 deploy_revision "/opt/wikibacon" do
 	repo "git://github.com/tedder/wikibacon.git"
