4213815d8b289efb7355d7cabf1b55d32458079b
tedder <ted@adhoclabs.co>
2013-04-28 17:44:57 -0700

    no can haz database.yml

--- wikibot_all_project_pages/recipes/deploy_wikibacon.rb
+++ wikibot_all_project_pages/recipes/deploy_wikibacon.rb
@@ -16,5 +16,6 @@ end
 deploy_revision "/opt/wikibacon" do
 	repo "git://github.com/tedder/wikibacon.git"
 	symlinks("bin" => "bin")
+	symlink_before_migrate nil # we don't have config/database.yml
 	#notifies
 end
